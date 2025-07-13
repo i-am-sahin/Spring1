f989c3369d463eeecdb674187596eac596384c74
Sahin Islam <sahinislam878@gmail.com>
2025-07-14 02:14:30 +0530

    Progress: Completed Spring 1st Project Lecture from Udemy

---
+++ src/main/java/com/sahin/Alien.java
@@ -0,0 +1,11 @@
+package com.sahin;
+
+import org.springframework.stereotype.Component;
+
+
+public class Alien {
+
+    void code(){
+        System.out.println("Coding!");
+    }
+}
---
+++ src/main/java/com/sahin/App.java
@@ -0,0 +1,24 @@
+package com.sahin;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//        System.out.println( "Hello World!" );
+        Alien alien = new Alien();
+        alien.code();
+
+
+        //Creating Container that stores bean
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(); //There is a different way to configuring Spring Project And one of the way is XML
+        Alien a2 = (Alien) applicationContext.getBean("Alien");
+
+    }
+}
