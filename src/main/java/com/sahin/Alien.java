60dc58b71faaed22b87be39e90b7c1ce63a07632
Sahin Islam <sahinislam878@gmail.com>
2025-07-14 15:28:09 +0530

    Progress: Completed Setter Injection Lecture from Udemy

--- src/main/java/com/sahin/Alien.java
+++ src/main/java/com/sahin/Alien.java
@@ -4,7 +4,16 @@ import org.springframework.stereotype.Component;
 
 
 public class Alien {
-    int age;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        System.out.println("Setting the Age");
+    }
 
     public Alien(){
         System.out.println("Object Created!");
--- src/main/java/com/sahin/App.java
+++ src/main/java/com/sahin/App.java
@@ -20,11 +20,11 @@ public class App
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml"); //There is a different way to configuring Spring Project And one of the way is XML
         Alien a2 = (Alien) applicationContext.getBean("alien");  // applicationContext.getBean --> Returns Object, so we've to typecast this.
 //        a2.code();
-        a2.age = 21;
-        System.out.println(a2.age);
-        Alien a3 = (Alien) applicationContext.getBean("alien");  // applicationContext.getBean --> Returns Object, so we've to typecast this.
-//        a2.code();
-        System.out.println(a3.age);
+//        a2.setAge(21);
+        System.out.println(a2.getAge());
+//        Alien a3 = (Alien) applicationContext.getBean("alien");  // applicationContext.getBean --> Returns Object, so we've to typecast this.
+////        a2.code();
+//        System.out.println(a3.age);
 
     }
 }
