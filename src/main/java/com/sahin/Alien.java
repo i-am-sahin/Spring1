3b780b4af596dd8af86df8b9f58ada0092e02d98
Sahin Islam <sahinislam878@gmail.com>
2025-07-14 15:52:33 +0530

    Progress: Completed Ref Attribute Lecture from Udemy

--- src/main/java/com/sahin/Alien.java
+++ src/main/java/com/sahin/Alien.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 public class Alien {
     private int age;
+    private Laptop lap;
 
     public int getAge() {
         return age;
@@ -15,6 +16,14 @@ public class Alien {
         System.out.println("Setting the Age");
     }
 
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop laptop) {
+        this.lap = laptop;
+    }
+
     public Alien(){
         System.out.println("Object Created!");
 
@@ -22,5 +31,6 @@ public class Alien {
 
     void code(){
         System.out.println("Coding!");
+        lap.compiling();
     }
 }
