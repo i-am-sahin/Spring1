7135543563b538c94ce44b8d5ab80fe9a7e858fb
Sahin Islam <sahinislam878@gmail.com>
2025-07-15 15:58:26 +0530

    Progress: Completed Java Based Configuration Lecture from Udemy

--- src/main/java/com/sahin/App.java
+++ src/main/java/com/sahin/App.java
@@ -1,6 +1,8 @@
 package com.sahin;
 
+import com.sahin.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -11,17 +13,36 @@ public class App
 {
     public static void main( String[] args )
     {
+
+//           Crating Bean container using Java Based Configuration
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Desktop dt = context.getBean(Desktop.class);
+        dt.compiling();
+
+
+
+
+
+
+
+
+
+
+
+
+
 //        System.out.println( "Hello World!" );
 //        Alien alien = new Alien();
 //        alien.code();
 
 
-        //Creating Container that stores bean
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml"); //There is a different way to configuring Spring Project And one of the way is XML
-//        Alien a2 = (Alien) applicationContext.getBean("alien");  // applicationContext.getBean --> Returns Object, so we've to typecast this.
-        Alien a2 =  applicationContext.getBean("alien",Alien.class);
-        a2.code();
-        System.out.println(a2.getAge());
+        //Creating Container that stores bean Using XML
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml"); //There is a different way to configuring Spring Project And one of the way is XML
+////        Alien a2 = (Alien) applicationContext.getBean("alien");  // applicationContext.getBean --> Returns Object, so we've to typecast this.
+//        Alien a2 =  applicationContext.getBean("alien",Alien.class);
+//        a2.code();
+//        System.out.println(a2.getAge());
 //        a2.setAge(21);
 //        System.out.println(a2.getAge());
 //        Alien a3 = (Alien) applicationContext.getBean("alien");  // applicationContext.getBean --> Returns Object, so we've to typecast this.
---
+++ src/main/java/com/sahin/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.sahin.config;
+
+import com.sahin.Desktop;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Desktop desktop(){
+           return new Desktop();
+    }
+}
