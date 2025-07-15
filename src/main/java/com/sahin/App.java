82b17e46f3113413385fd005312fcd204261b62a
Sahin Islam <sahinislam878@gmail.com>
2025-07-15 15:38:50 +0530

    Progress: Completed GetBean By Type Lecture from Udemy

--- src/main/java/com/sahin/App.java
+++ src/main/java/com/sahin/App.java
@@ -18,7 +18,8 @@ public class App
 
         //Creating Container that stores bean
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml"); //There is a different way to configuring Spring Project And one of the way is XML
-        Alien a2 = (Alien) applicationContext.getBean("alien");  // applicationContext.getBean --> Returns Object, so we've to typecast this.
+//        Alien a2 = (Alien) applicationContext.getBean("alien");  // applicationContext.getBean --> Returns Object, so we've to typecast this.
+        Alien a2 =  applicationContext.getBean("alien",Alien.class);
         a2.code();
         System.out.println(a2.getAge());
 //        a2.setAge(21);
@@ -27,7 +28,10 @@ public class App
 ////        a2.code();
 //        System.out.println(a3.age);
 
-        Desktop desktop = (Desktop) applicationContext.getBean("com1");
+
+                //If we are not spicing the name here, then it will start searching by type.
+        Desktop desktop =  applicationContext.getBean(Desktop.class);
+        Computer com = applicationContext.getBean(Computer.class);
 
     }
 }
